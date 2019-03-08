01e0205fa196ed49b0d407596e1615d131a8b3b7
ShadeP <pettewayshade@gmail.com>
2019-03-08 14:09:40 -0600

    first working version

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,38 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// button
+var submit = d3.select("#filter-btn");
+
+// func to create table
+function createTable(objects) {
+    var tbody = d3.select("tbody");
+    tbody.html("")
+    objects.forEach((element) => {
+        var tr = tbody.append("tr");
+        Object.entries(element).forEach(([key, value]) => {
+            var cell = tr.append("td");
+            cell.text(value)
+        })
+    })
+}
+
+// func to get values to filter on
+function filterOn(object) {
+    filterKeys = ["datetime", "city", "country", "state", "shape"]
+    var matched = false;
+    filterKeys.forEach((key) => {
+        filter = d3.select(`#${key}`).property("value");
+        if (filter === object[key]) {matched = true}
+    })
+    return matched
+}
+
+// func to run when submit button is clicked
+submit.on("click", function() {
+    // prevents page from refreshing
+    d3.event.preventDefault()
+    // filter the table
+    filteredData = tableData.filter(filterOn)
+    createTable(filteredData)
+})
